5b3bbde2be11bd3af2538a59c239cd20e1b4feaa
Charlie Pelton <charliejp0311@gmail.com>
2020-10-12 16:51:55 -0500

    Done.

--- .history/src/App_20201012164556.js
+++ .history/src/App_20201012164556.js
@@ -5,12 +5,12 @@ import { addItem } from  './actions/items';
 
 class App extends Component {
 
-  handleOnClick = event => {
-    this.props.addItem()
+  handleOnClick() {
+    this.props.store.dispatch(addItem());
   }
 
   render() {
-    // debugger
+    debugger
     return (
       <div className="App">
         <button onClick={this.handleOnClick}>
@@ -22,16 +22,18 @@ class App extends Component {
   }
 };
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
     items: state.items
-  }
-}
+  };
+};
 
 const mapDispatchToProps = dispatch => {
   return {
-    addItem: () => { dispatch(addItem()) }
-  }
-}
+    addItem: () => {
+      dispatch(addItem())
+    }
+  };
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
